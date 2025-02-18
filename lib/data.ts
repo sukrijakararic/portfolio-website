eb6c599a215af9da295ed28110171470bb22c24c
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-18 21:54:05 +0100

    Add project site links to project components

--- lib/data.ts
+++ lib/data.ts
@@ -63,6 +63,7 @@ export const experiencesData = [
 export const projectsData = [
   {
     title: "CorpComment",
+    site: "https://youtube.com",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
@@ -70,6 +71,7 @@ export const projectsData = [
   },
   {
     title: "rmtDev",
+    site: "https://github.com",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
@@ -77,6 +79,7 @@ export const projectsData = [
   },
   {
     title: "Word Analytics",
+    site: "https://github.com",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],

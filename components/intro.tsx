60b02e747643189cbe10238203f78d45c01cff5f
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-19 11:48:40 +0100

    Update About and Intro components with new personal details and links

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/sukrijakararic"
+          href="https://www.linkedin.com/in/sukrija-kararic-20a57b270/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/sukrijakararic"
           target="_blank"
         >
           <FaGithubSquare />

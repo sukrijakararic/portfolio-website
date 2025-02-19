fcf5c875ff56a838590fed5456e3a6535039a117
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-19 13:23:55 +0100

    Update experience description in Intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Shuki.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy
+        <span className="font-bold">3 years</span> of Programming experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (PERN)</span>.
       </motion.h1>
